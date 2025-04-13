c64c606907c242cc781f2f2e0484e465160deb03
Abhaya Kumar prajapati <110589578+abhayakumarprajapati@users.noreply.github.com>
2025-04-13 12:29:38 +0530

    Update app.js

--- app.js
+++ app.js
@@ -18,32 +18,32 @@ app.get('/', (req, res) => {
     res.send("hello")
 })
 
-app.get('/products', async (req, res) => {
+// app.get('/products', async (req, res) => {
 
-    // const isExist = await redis.exists("products")
-    let products = await redis.get("products")
+//     // const isExist = await redis.exists("products")
+//     let products = await redis.get("products")
 
-    if (products) {
-        console.log('get from cache')
-
-
-        return res.json({
-            products: JSON.parse(products)
-        })
-
-    }
+//     if (products) {
+//         console.log('get from cache')
 
 
-    products = await getProducts()
+//         return res.json({
+//             products: JSON.parse(products)
+//         })
 
-    // await redis.set("products",JSON.stringify(products))
-    await redis.setex("products", 20, JSON.stringify(products))
+//     }
 
 
-    res.json({
-        products
-    })
-})
+//     products = await getProducts()
+
+//     // await redis.set("products",JSON.stringify(products))
+//     await redis.setex("products", 20, JSON.stringify(products))
+
+
+//     res.json({
+//         products
+//     })
+// })
 
 
 app.listen(3000, () => {
